d2da5e7bb14f39235988c74d02d5ba433fa52d9b
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2024-12-25 03:04:37 +0100

    DB_SKIP_SSL env var

--- rootfs/etc/cont-init.d/03-config.sh
+++ rootfs/etc/cont-init.d/03-config.sh
@@ -62,6 +62,7 @@ DB_USER=${DB_USER:-flarum}
 DB_PREFIX=${DB_PREFIX:-flarum_}
 DB_NOPREFIX=${DB_NOPREFIX:-false}
 DB_TIMEOUT=${DB_TIMEOUT:-60}
+DB_SKIP_SSL=${DB_SKIP_SSL:-true}
 
 # Timezone
 echo "Setting timezone to ${TZ}..."
@@ -125,7 +126,10 @@ if [ -z "$DB_PASSWORD" ]; then
   echo >&2 "ERROR: Either DB_PASSWORD or DB_PASSWORD_FILE must be defined"
   exit 1
 fi
-dbcmd="mysql --skip-ssl -h ${DB_HOST} -P ${DB_PORT} -u "${DB_USER}" "-p${DB_PASSWORD}""
+dbcmd="mariadb -h ${DB_HOST} -P ${DB_PORT} -u "${DB_USER}" "-p${DB_PASSWORD}""
+if [ "$DB_SKIP_SSL" = "true" ]; then
+  dbcmd="$dbcmd --skip-ssl"
+fi
 
 echo "Waiting ${DB_TIMEOUT}s for database to be ready..."
 counter=1
